3a1480be1a68a5254054d72b7cb66f5fcd6ee966
Your Name <you@example.com>
2018-03-21 14:49:44 +0600

    Inheritance Checking

---
+++ Calculation.java
@@ -0,0 +1,23 @@
+package inheritance_practice01;
+
+
+public class Calculation {
+	int z;
+	
+	public void addition (int x, int y) {
+		z= x+y;
+		System.out.println("The Sum of the Given Number:"+z);
+		}
+	public void substraction (int x, int y){
+		z=x+y;
+		System.out.println("The difference between given 2 numbers:"+z);
+	}
+	
+}
+
+
+		
+	
+
+
+	
